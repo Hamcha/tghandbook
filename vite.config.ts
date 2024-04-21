5949fdec680997287359e19bf759389057a8c1b2
Hamcha <hamcha@crunchy.rocks>
2024-04-21 14:47:34 +0200

    prevent pwa-ifying until we fix stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import path from "path";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+//import { VitePWA } from "vite-plugin-pwa";
 import changelog from "./plugins/changelog";
 
 export default defineConfig({
   plugins: [
     changelog(),
-    VitePWA({
+    /*VitePWA({
       devOptions: {
         enabled: true,
       },
-    }),
+    }),*/
   ],
   resolve: {
     alias: {
